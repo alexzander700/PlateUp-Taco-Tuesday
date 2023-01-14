ce284e49926a25375672860d7177c3e6268f84ed
alexzander700 <ajhochreiter@gmail.com>
2023-01-14 12:22:20 -0500

    Add project files.

---
+++ PlateUp Taco Tuesday/Mod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using KitchenMods;
+using UnityEngine;
+using KitchenLib;
+using KitchenLib.References;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+
+namespace PlateUp_Taco_Tuesday
+{
+    class Mod : BaseMod, IModSystem
+    {
+        internal const string MOD_ID = "TacoTuesday";
+        internal const string MOD_NAME = "Taco Tuesday";
+        internal const string MOD_VERSION = "1.0.0";
+        internal const string MOD_AUTHOR = "alexzander700";
+        internal const string PLATEUP_VERSION = "1.1.2";
+
+        private void AddRecipies()
+        {
+            
+        }
+
+        
+
+
+    }
+}
