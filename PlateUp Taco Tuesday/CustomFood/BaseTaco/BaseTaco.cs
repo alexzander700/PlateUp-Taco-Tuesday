ae4b524b57833e276c124dfd457e589236ee444d
alexzander700 <ajhochreiter@gmail.com>
2023-01-30 10:07:26 -0500

    base systems

---
+++ PlateUp Taco Tuesday/CustomFood/BaseTaco/BaseTaco.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using KitchenLib.Customs;
+using System.Collections.Generic;
+using KitchenData;
+using KitchenMods;
+
+namespace TacoTuesday.CustomFood.BaseTaco
+{
+    public class BaseTaco : CustomDish
+    {
+        public override string UniqueNameID => "Taco -- Base";
+        public override DishType Type => DishType.Base;
+        public override DishCustomerChange CustomerMultiplier => DishCustomerChange.SmallDecrease;
+        public override CardType CardType => CardType.Default;
+        public override Unlock.RewardLevel ExpReward => UnlockGroup.RewardLevel.Medium;
+        public override UnlockGroup UnlockGroup => UnlockGroup.Dish;
+        public override List<Dish.MenuItem> ResultingMenuItems => new List<Dish.MenuItem>
+        {
+            new Dish.MenuItem
+            {
+                Item = Mod.PlatedHardBeefTaco
+            }
+        };
+        public override HashSet<Dish.IngredientUnlock> IngredientUnlocks => new HashSet<Dish.IngredientUnlock>
+        {
+            new Dish.IngredientUnlock
+            {
+                Ingredient = Mod.
+                MenuItem = Mod.PlatedHardBeefTaco
+            }
+        };
+
+
+
+    }
+}
--- PlateUp Taco Tuesday/Mod.cs
+++ PlateUp Taco Tuesday/Mod.cs
@@ -11,24 +11,56 @@ using KitchenLib;
 using KitchenLib.References;
 using KitchenLib.Customs;
 using KitchenLib.Utils;
+using KitchenData;
+using ItemReference = KitchenLib.References.ItemReferences;
 
 namespace PlateUp_Taco_Tuesday
 {
     class Mod : BaseMod, IModSystem
     {
+        internal const string MOD_GUID = "mod.alexzander700.plateup.tacotuesday";
         internal const string MOD_ID = "TacoTuesday";
         internal const string MOD_NAME = "Taco Tuesday";
         internal const string MOD_VERSION = "1.0.0";
         internal const string MOD_AUTHOR = "alexzander700";
         internal const string PLATEUP_VERSION = "1.1.2";
 
+        internal static Item Cheese => GetExistingGDO<Item>(ItemReference.Cheese);
+
+        internal static Process Cook => GetExistingGDO<Process>(ProcessReferences.Cook);
+        internal static Process Chop => GetExistingGDO<Process>(ProcessReferences.Chop);
+
+        internal static Item Cheese => GetExistingGDO<Item>(ItemReference.Cheese);
         private void AddRecipies()
         {
             
         }
 
-        
 
 
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+
+            AddGameDataObject<Tortilla>();
+            AddGameDataObject<TortillaShell>();
+            AddGameDataObject<TortillaChips>();
+
+            AddGameDataObject<BeefHardTaco>();
+        }
+
+
+        private static T1 GetModdedGDO<T1, T2>() where T1 : GameDataObject
+        {
+            return (T1)GDOUtils.GetCustomGameDataObject<T2>().GameDataObject;
+        }
+        private static T GetExistingGDO<T>(int id) where T : GameDataObject
+        {
+            return (T)GDOUtils.GetExistingGDO(id);
+        }
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, PLATEUP_VERSION, Assembly.GetExecutingAssembly()) { }
     }
+
+
 }
